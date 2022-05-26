65254b0016cb3ffb22101968208b0d954ace9014
Galun <1353663350@qq.com>
2022-05-26 21:32:31 +0800

    更新drom-dromitory模块

---
+++ dorm-admin/src/test/java/com/joy/dorm/BuildingTest.java
@@ -0,0 +1,23 @@
+package com.joy.dorm;
+
+import dao.IBuildingDao;
+import dao.impl.BuildingDaoImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import service.IBuildingService;
+
+@SpringBootTest
+public class BuildingTest {
+
+
+//    @Autowired
+//    private IBuildingService buildingService;
+//
+//    @Test
+//    public void findBuildingsTest(){
+//        System.out.println(buildingService.getBuildings());
+//    }
+
+}
