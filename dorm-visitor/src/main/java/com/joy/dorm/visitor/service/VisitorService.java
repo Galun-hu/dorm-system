c4cb91e8c31d992af785bc6e887dc3618b43e46e
liping <2655405368@qq.com>
2022-06-08 00:40:07 +0800

    修改注释

--- dorm-visitor/src/main/java/com/joy/dorm/visitor/service/VisitorService.java
+++ dorm-visitor/src/main/java/com/joy/dorm/visitor/service/VisitorService.java
@@ -14,38 +14,38 @@ public class VisitorService {
     @Autowired
     VisitorDaoImpl visitorDao;
 
-    //获取宿舍管理员角色维修记录分页数据
+    //获取宿舍管理员角色访客记录分页数据
     public List<Visitor> getAllVisitor(String keywords, Integer id,long pageNumNew,long pageSize) {
         return visitorDao.getAllVisitor(keywords,id,pageNumNew,pageSize);
     }
 
-    //添加维修
+    //添加访客
     public int addVisitor(Visitor visitor) {
         visitor.setCreateTime(new Date());
         return visitorDao.addVisitor(visitor);
     }
 
-    //修改报修
+    //修改访客信息
     public int updateVisitor(Visitor visitor) {
         return visitorDao.updateVisitor(visitor);
     }
 
-    //删除报修
+    //删除访客
     public int deleteVisitor(Integer id) {
         return visitorDao.deleteVisitor(id);
     }
 
-    //获取分页总记录数
+    //获取楼栋分页总记录数
     public Long getVisitorCount(String keywords, Integer id) {
         return visitorDao.getVisitorCount(keywords,id);
     }
 
-    //获取系统管理员维修分页总记录数
+    //获取该楼栋访客分页总记录数
     public Long getVisitorAdminCount(String keywords,Integer buildingId) {
         return visitorDao.getVisitorAdminCount(keywords,buildingId);
     }
 
-    //获取系统管理员维修分页数据
+    //获取该楼栋访客分页数据
     public List<Visitor> getAllVisitorAdmin(String keywords,long pageNumNew,long pageSize,Integer buildingId){
         return visitorDao.getAllVisitorAdmin(keywords,pageNumNew,pageSize,buildingId);
     }
